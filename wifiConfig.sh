9388edbbe812ae1a95d9d33d4a19451a97e009c6
Ramon <user1@server1.fake1>
2013-09-25 08:25:44 -0400

    Configure dhcp

--- wifiConfig.sh
+++ wifiConfig.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 #install hostapd and dhcp server
 sudo apt-get -y install hostapd isc-dhcp-server
+
 #configure isc-dhcp-server
-if [ diff 
+if [ diff /etc/default/isc-dhcp-server isc-dhcp-server ];then
+	mv /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.ORIG
+	cp isc-dhcp-server /etc/default/isc-dhcp-server  
+	echo "isc-dhcp-server settings modified!"
+fi
+if [ diff /etc/dhcp/dhcpd.conf dhcpd.conf ];then
+	mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.ORIG
+	cp dhcpd.conf /etc/dhcp/dhcpd.conf  
+	echo "isc-dhcp-server settings modified!"
+fi
+
 
 
