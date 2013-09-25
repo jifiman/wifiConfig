bc5a1b54cc7883dc570a72f39dac92f7f8052879
Ramon <user1@server1.fake1>
2013-09-25 08:31:28 -0400

    Fixed binary operator error in wifiConfig.sh

--- wifiConfig.sh
+++ wifiConfig.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+set -x
 #install hostapd and dhcp server
 sudo apt-get -y install hostapd isc-dhcp-server
 
 #configure isc-dhcp-server
-if [ diff /etc/default/isc-dhcp-server isc-dhcp-server ];then
+if [ 'diff /etc/default/isc-dhcp-server isc-dhcp-server' ];then
 	mv /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.ORIG
 	cp isc-dhcp-server /etc/default/isc-dhcp-server  
 	echo "isc-dhcp-server settings modified!"
 fi
-if [ diff /etc/dhcp/dhcpd.conf dhcpd.conf ];then
+if [ 'diff /etc/dhcp/dhcpd.conf dhcpd.conf' ];then
 	mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.ORIG
 	cp dhcpd.conf /etc/dhcp/dhcpd.conf  
 	echo "isc-dhcp-server settings modified!"
